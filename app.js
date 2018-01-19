7c443ccbec1eeca84bb97191322b9ffd2331e958
shellwayne01 <shellwayne01@gmail.com>
2018-01-19 16:02:43 -0500

    updated app

--- app.js
+++ app.js
@@ -6,12 +6,49 @@ console.log("sever is starting");
 app.use(express.static('views')); 
 
 
-app.get('/search/:genre', function(req, res){
+var server = app.listen(8080, listening);
+
+function listening(){
+    console.log("listening");
+}
+
+//var genres = [horror, "romance", "science fiction", "comedy", "biography", "non-fiction", "fiction"];
+
+var data = {
+    bookstore:[
+    {
+    book: {
+        title: "Harry Potter",
+        genre: "science fiction"
+    }},
+    
+    {
+    book: {
+        title: "Pride and Prejudice",
+        genre: "romance"
+    }} 
+    ]
+      
+};
+
+
+app.get('/search/:genre', chooseGenre);
+
+function chooseGenre(request, response){
     var genre = request.params.genre;
     console.log(genre);
-    res.render('bookfinder.ejs', {genre: genre});
-    }
-    );
- 
+    response.render('bookfinder.ejs', {genre: genre, bookstore: data.bookstore});
+}
 
-.listen(8080);
+
+
+
+
+
+
+//app.get('/search/:genre', function(req, res){
+//    var genre = request.params.genre;
+//    console.log(genre);
+//    res.render('bookfinder.ejs', {genre: genre});
+//    })
+// 
