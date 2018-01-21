965cee7b4c3d6f92c96a04a3198803c38905776d
shellwayne01 <shellwayne01@gmail.com>
2018-01-21 17:52:06 -0500

    updated app

--- app.js
+++ app.js
@@ -16,17 +16,36 @@ function listening(){
 
 var data = {
     bookstore:[
-    {
-    book: {
-        title: "Harry Potter",
-        genre: "science fiction"
-    }},
     
-    {
-    book: {
+    book = {
+        title: "Harry Potter",
+        genre: "science fiction",
+        cover: "https://ryan1bowen.files.wordpress.com/2015/01/harry-potter-books.jpg"
+    },
+    
+    book = {
         title: "Pride and Prejudice",
-        genre: "romance"
-    }} 
+        genre: "romance",
+        cover: "http://www.g-pop.net/images/pridenovel.jpg"
+    },
+    
+    book = {
+        title: "The Alchemist",
+        genre: "fiction",
+        cover: "https://fictivecharacters.files.wordpress.com/2010/11/the_alchemist.jpg"
+    },
+        
+    book = {
+        title: "The Picture of Dorian Gray",
+        genre: "fiction",
+        cover: "http://ecx.images-amazon.com/images/I/41cUbBszshL.jpg"
+    },
+        
+    book = {
+        title: "Memoirs of a Geisha",
+        genre: "historical novel",
+        cover: "https://currentread.files.wordpress.com/2011/04/memoirs.jpg"
+    }    
     ]
       
 };
@@ -36,8 +55,22 @@ app.get('/search/:genre', chooseGenre);
 
 function chooseGenre(request, response){
     var genre = request.params.genre;
-    console.log(genre);
-    response.render('bookfinder.ejs', {genre: genre, bookstore: data.bookstore});
+    //console.log(genre);
+    
+    //Retrieves books of desired genre
+    var bookstore = data.bookstore;
+    var relevantBooks = [];
+    
+    for(i=0; i<bookstore.length; i++){ 
+        if (genre == bookstore[i].genre){ //bookstore[i] is just a book
+            relevantBooks.push(bookstore[i]);
+        }
+    }
+    console.log(relevantBooks);
+    
+    response.render('bookfinder.ejs', {genre: genre, 
+                                       matchedBooks: relevantBooks });
+    //console.log(JSON.stringify(data.bookstore));
 }
 
 
